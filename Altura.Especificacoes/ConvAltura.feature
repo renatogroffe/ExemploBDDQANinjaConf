e4b62a1aee413c9f179f7984d00a02da3f9686e2
Renato Groffe <renato.groffe@yahoo.com.br>
2016-10-23 23:31:39 -0200

    Add project files.

---
+++ Altura.Especificacoes/ConvAltura.feature
@@ -0,0 +1,16 @@
+﻿Funcionalidade: Conversão de alturas em pés para metros
+
+Cenário: Cenario01
+	Dado que foi informada uma altura de 1 pé(s)
+	Quando for solicitada a conversão deste valor
+	Então o valor equivalente será de 0,3048 metro(s)
+
+Cenário: Cenario02
+	Dado que foi informada uma altura de 10 pé(s)
+	Quando for solicitada a conversão deste valor
+	Então o valor equivalente será de 3,048 metro(s)
+
+Cenário: Cenario03
+	Dado que foi informada uma altura de 100 pé(s)
+	Quando for solicitada a conversão deste valor
+	Então o valor equivalente será de 30,48 metro(s)
---
+++ Altura.Especificacoes/ConvAlturaStepDefinition.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Altura.Especificacoes
+{
+    [Binding]
+    public sealed class ConvAlturaStepDefinition
+    {
+        private double _pes;
+        private double _metros;
+
+        [Given("que foi informada uma altura de (.*) pé\\(s\\)")]
+        public void PreencherAlturaPes(double pes)
+        {
+            _pes = pes;
+        }
+
+        [When("for solicitada a conversão deste valor")]
+        public void ProcessarConversaoAltura()
+        {
+            _metros = ConversorAltura.PesParaMetros(_pes);
+        }
+
+        [Then("o valor equivalente será de (.*) metro\\(s\\)")]
+        public void VerificarCalculoMetros(double metros)
+        {
+            Assert.AreEqual(metros, _metros);
+        }
+    }
+}
